207dbdee216d9a015ad6c779bf7b833f71f3986e
gaelidone <gaelidonesarmiento@gmail.com>
2024-06-07 00:08:20 -0300

    avancé con componentes TodayHighlights e InfoHighl

--- src/components/InfoHighlights.jsx
+++ src/components/InfoHighlights.jsx
@@ -1,8 +1,8 @@
 export function AirQuality({ }) {
   return (
     <div className="rounded-xl bg-blackPri p-2 w-[95%] mx-auto">
-      <div className="flex mb-1">
-        <p className="text-gray-400 text-sm">Air Quality Index</p>
+      <div className="flex mb-2">
+        <p className="text-gray-400">Air Quality Index</p>
       </div>
       <div className="flex justify-between text-xl items-center">
         <i className="bi bi-wind text-2xl"></i>
@@ -26,6 +26,42 @@ export function AirQuality({ }) {
             <p className="text-gray-400 text-sm">PM25</p>
           </div>
         </div>
+      </div>
+    </div>
+  )
+}
+
+export function Sunrise({ }) {
+  return (
+    <div className="rounded-xl bg-blackPri p-2 w-[95%] mx-auto">
+      <p className="text-gray-400 mb-2">Sunrise & Sunset</p>
+      <div className="flex justify-around items-center px-2">
+        <div className="flex gap-3 items-center">
+          <i className="bi bi-sun text-2xl"></i>
+          <div className="flex flex-col">
+            <p className="text-sm text-gray-400">Sunrise</p>
+            <p className="text-lg">6:47 AM</p>
+          </div>
+        </div>
+        <div className="flex gap-3 items-center">
+          <i className="bi bi-moon text-2xl"></i>
+          <div className="flex flex-col">
+            <p className="text-sm text-gray-400">Sunset</p>
+            <p className="text-lg">6:20 PM</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function CardInfo({ }) {
+  return(
+    <div className="flex flex-col gap-2 rounded-xl bg-blackPri p-2 w-[95%] mx-auto">
+      <p className="text-gray-400">Humidity</p>
+      <div className="flex justify-between px-1">
+        <i className="bi bi-droplet text-2xl"></i>
+        <p className="text-xl">56%</p>
       </div>
     </div>
   )
--- src/components/TodayHighlights.jsx
+++ src/components/TodayHighlights.jsx
@@ -1,11 +1,14 @@
-import { AirQuality } from "./InfoHighlights";
+import { AirQuality, Sunrise, CardInfo } from "./InfoHighlights";
 
 function TodayHighlights({ }) {
   return (
-    <section className="card-sect mt-4 max-w-[92.5%] flex-col gap-1">
-      <h3>Todays Highlights</h3>
+    <section className="card-sect mt-4 max-w-[92.5%] flex-col gap-2">
+      <h3 className="font-bold">Todays Highlights</h3>
       <div className="flex flex-col gap-4">
         <AirQuality />
+        <Sunrise />
+        <CardInfo />
+        <CardInfo />
       </div>
     </section>
   )
